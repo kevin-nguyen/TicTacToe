8cfe751e8759475ab6eb491aeebdf71c89a32cd8
Kevin Nguyen <threekevinnguyen@gmail.com>
2015-11-13 00:06:22 -0800

    Change the color scheme of the app

--- logic.js
+++ logic.js
@@ -1,3 +1,19 @@
 $(document).ready(function () {
-    
+    var state = 0;
+
+    $('.content').click(function(event) {
+        var val = $(this).text();
+
+        if (val !== '') {
+            return;
+        }
+
+        if (state) {
+            $(this).text('X');
+            state = 0;
+        } else {
+            $(this).text('O');
+            state = 1;
+        }
+    });
 });
